b9929c1dc060b8ddef64f4dd836c02d11766392c
Corey Burkett <coreyburkett22@gmail.com>
2020-10-18 19:25:18 -0700

    completed service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,6 @@
 console.log("This is the service worker: ");
 
-const CACHE_NAME = "static-cache-v1";
+const CACHE_NAME = "static-cache-v3";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
@@ -13,3 +13,63 @@ const FILES_TO_CACHE = [
     "/icons/icon-512x512.png",
 ];
 
+self.addEventListener("install", function(evt) {
+    //general listener use this or the other two
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then(cacheObj => {
+            console.log("Cached successfully!");
+            return cacheObj.addAll(FILES_TO_CACHE);
+        })
+    );
+
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event) {
+    event.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old key", key);
+                        return caches.delete(key);
+                    }
+                })
+            )
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event) {
+    if (event.request.url.includes("/api/")) {
+        event.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(event.request)
+                .then(response => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+
+                    return response;
+                })
+                .catch(error => {
+                    return cache.match(event.request.url);
+                });
+            }).catch(error => console.log(error))
+        )
+        return;
+    }
+
+    event.respondWith(
+        caches.match(event.request.url)
+        .then(response => {
+            return response || fetch(event.request);
+        })
+    )
+});
--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require("path");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,11 +18,17 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
